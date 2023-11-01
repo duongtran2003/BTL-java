a70cd3b7ba7797abb8c408b848ef8d5564d43c86
duongtran2003 <dt005250@gmail.com>
2023-11-01 20:04:12 +0700

    initial commit

--- src/main/java/dal/DAO/DAO.java
+++ src/main/java/dal/DAO/DAO.java
@@ -8,6 +8,35 @@ package dal.DAO;
  *
  * @author pc
  */
-public class DAO {
-	
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+/**
+ *
+ * @author admin
+ */
+abstract class DAO {
+    protected static Connection con ;
+    public DAO (){
+        if(con == null){
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                String url = "jdbc:mysql://localhost:3306/test?autoReconnect=true&useSSL=false";
+                String username = "root";
+                String password = "Luongngocyen15012003@";
+                con = DriverManager.getConnection(url,username,password);
+            }
+            catch(Exception e){
+                System.out.println("FAIL");
+            }
+        }
+    }
+    public abstract Object getById(int Record_id);
+    public abstract void addObject(Object object);
+    public abstract void updateObject(Object object);
+    public abstract void deleteObject(int objectId);
+    public abstract List<Object> getAllObjects();
 }
