8de04fb1296c5c24a3efb0b48977100c686c01cb
duongtran2003 <dt005250@gmail.com>
2023-11-17 21:21:13 +0700

    refractor

--- src/main/java/common/product/Constant.java
+++ src/main/java/common/product/Constant.java
@@ -9,17 +9,17 @@ package common.product;
  * @author DELL
  */
 public class Constant {
-    public static final String URL_VOUCHER_ADD= "product/voucher/add";
-    public static final String URL_VOUCHER_DELETE= "product/voucher/delete";
-    public static final String URL_VOUCHER_GET= "product/voucher/get";
+    public static final String URL_VOUCHER_ADD= "/product/voucher/add";
+    public static final String URL_VOUCHER_DELETE= "/product/voucher/delete";
+    public static final String URL_VOUCHER_GET= "/product/voucher/get";
     
-    public static final String URL_HAS_VOUCHER_ADD= "product/hasvoucher/add";
-    public static final String URL_HAS_VOUCHER_DELETE= "product/hasvoucher/delete";
-    public static final String URL_HAS_VOUCHER_GET= "product/hasvoucher/get";
+    public static final String URL_HAS_VOUCHER_ADD= "/product/hasvoucher/add";
+    public static final String URL_HAS_VOUCHER_DELETE= "/product/hasvoucher/delete";
+    public static final String URL_HAS_VOUCHER_GET= "/product/hasvoucher/get";
     
     
-    public static final String URL_ORDER_ADD="product/order/createOrder";
-    public static final String URL_ORDER_DELETE="product/order/cancelOrder";
-    public static final String URL_ORDER_GET="product/order/getOrder";
+    public static final String URL_ORDER_ADD="/product/order/createOrder";
+    public static final String URL_ORDER_DELETE="/product/order/cancelOrder";
+    public static final String URL_ORDER_GET="/product/order/getOrder";
     
 }
