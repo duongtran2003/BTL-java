b753960d11379823950ee51411b8219d4aac233f
son023 <sonnguyenvan122003@gmail.com>
2023-11-18 07:00:41 +0700

    updatecontrollervoucher,hasvoucher

--- src/main/java/controllers/product/OperateWithHasVoucher.java
+++ src/main/java/controllers/product/OperateWithHasVoucher.java
@@ -18,82 +18,32 @@ import java.text.*;
 import java.sql.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.JSONObject;
 
 /**
  *
  * @author DELL
  */
 
-@WebServlet(name="OperateWithHasVoucher", urlPatterns={Constant.URL_HAS_VOUCHER_ADD,Constant.URL_HAS_VOUCHER_DELETE,Constant.URL_HAS_VOUCHER_GET})
+@WebServlet(name="HasVoucherServlet", urlPatterns={"product/hasvoucher"})
 public class OperateWithHasVoucher extends HttpServlet {
 
     private Gson gson=new Gson();
-    private SimpleDateFormat date=new SimpleDateFormat("yyyy/MM/dd");
     private HasVoucherDAO hasVoucherDAO=new HasVoucherDAO();
     private VoucherDAO voucherDAO=new VoucherDAO();
+    //add by id
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        try {
-            String urlPath=request.getServletPath();
-            switch (urlPath) {
-                case Constant.URL_HAS_VOUCHER_ADD:  
-                    addHasVoucher(request,response);          
-                    break;    
-                case Constant.URL_HAS_VOUCHER_DELETE:
-                    deleteHasVoucher(request,response);
-                    break;
-                case Constant.URL_HAS_VOUCHER_GET:
-                    getHasVoucher(request,response);
-                    break;
-                    
-            }
-        } catch (ParseException ex) {
-            Logger.getLogger(OperateWithHasVoucher.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    private void getHasVoucher(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        Map<String, Object> res = new HashMap<> ();
-        try {
-            int id=Integer.parseInt(request.getParameter("has_voucher_id"));
-            HasVoucher hasVoucher = (HasVoucher) hasVoucherDAO.getById(id);
-            JSONHelper.sendJsonAsResponse(response, 200, hasVoucher);
-        } 
-        catch (NumberFormatException e) {
-                res.put("message", "bad request, check url params again");
-                response.setStatus(400);
-                JSONHelper.sendJsonAsResponse(response, 400, res);
-        }
-    }
-    private void deleteHasVoucher(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        Map<String, Object> res = new HashMap<> ();
-        try {
-            int id=Integer.parseInt(request.getParameter("has_voucher_id"));
-            boolean isSuccess = hasVoucherDAO.deleteObject(id);
-            if (isSuccess) {
-                    res.put("message", "success");	
-                    JSONHelper.sendJsonAsResponse(response, 200, res);
-            }
-            else {
-                    res.put("message", "server's error");	
-                    JSONHelper.sendJsonAsResponse(response, 500, res);
-            }
-        } 
-        catch (NumberFormatException e) {
-                res.put("message", "bad request, check url params again");	
-                JSONHelper.sendJsonAsResponse(response, 400, res);
-        }
-        
-    }
-    private void addHasVoucher(HttpServletRequest request, HttpServletResponse response) throws IOException, ParseException{
         Map<String, Object> res = new HashMap<> ();
         try{
-            int id=Integer.parseInt(request.getParameter("has_voucher_id"));
-            int user_id=Integer.parseInt(request.getParameter("user_id"));
-            int voucher_id=Integer.parseInt(request.getParameter("voucher_id"));
+            StringBuilder json = (StringBuilder) request.getAttribute("json");
+            JSONObject jsonObject = new JSONObject(json.toString());
+            int user_id=jsonObject.getInt("user_id");
+            int voucher_id=jsonObject.getInt("voucher_id");
             Voucher voucher=(Voucher) voucherDAO.getById(voucher_id);
             Date expiration_date= new Date((long) voucher.getExpire_time()*24*60*60+System.currentTimeMillis());
-            HasVoucher hasVoucher= new HasVoucher(id, user_id,voucher_id, expiration_date);
+            HasVoucher hasVoucher= new HasVoucher(0, user_id,voucher_id, expiration_date);
             boolean isSuccess = hasVoucherDAO.addObject(hasVoucher);
             if (isSuccess) {
                     res.put("message", "success");	
@@ -108,9 +58,50 @@ public class OperateWithHasVoucher extends HttpServlet {
             res.put("message", "bad request, check url params again");	
             JSONHelper.sendJsonAsResponse(response, 400, res);
         }
-
+    }
+    // xoá by id
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> res = new HashMap<> ();
+        try {
+            StringBuilder json = (StringBuilder) req.getAttribute("json");
+            Gson gson = new Gson();
+            JSONObject jsonObject = new JSONObject(json.toString());
+            int id=jsonObject.getInt("has_voucher_id");
+            boolean isSuccess = hasVoucherDAO.deleteObject(id);
+            if (isSuccess) {
+                    res.put("message", "success");	
+                    JSONHelper.sendJsonAsResponse(resp, 200, res);
+            }else {
+                    res.put("message", "server's error");	
+                    JSONHelper.sendJsonAsResponse(resp, 500, res);
+            }
+        } 
+        catch (NumberFormatException e) {
+                res.put("message", "bad request, check url params again");	
+                JSONHelper.sendJsonAsResponse(resp, 400, res);
+        }
         
     }
+    // get by id
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> res = new HashMap<> ();
+        try {
+            int id=Integer.parseInt(req.getParameter("has_voucher_id"));
+            HasVoucher hasVoucher = (HasVoucher) hasVoucherDAO.getById(id);
+            JSONHelper.sendJsonAsResponse(resp, 200, hasVoucher);
+        } 
+        catch (NumberFormatException e) {
+                res.put("message", "bad request, check url params again");
+                resp.setStatus(400);
+                JSONHelper.sendJsonAsResponse(resp, 400, res);
+        }
+    }
+    
+    
+  
+
     @Override
     public String getServletInfo() {
         return "Short description";
--- src/main/java/controllers/product/OperateWithVoucher.java
+++ src/main/java/controllers/product/OperateWithVoucher.java
@@ -12,74 +12,30 @@ import common.product.Constant;
 import dal.ProductDAO.VoucherDAO;
 import helper.JSONHelper;
 import java.util.*;
+import org.json.JSONObject;
 
 /**
  *
  * @author DELL
  */
 
-@WebServlet(name="OperateWithVoucher", urlPatterns={Constant.URL_VOUCHER_ADD,Constant.URL_VOUCHER_DELETE,Constant.URL_VOUCHER_GET})
+@WebServlet(name="VoucherServlet", urlPatterns={"product/voucher"})
 public class OperateWithVoucher extends HttpServlet {
 
     private Gson gson=new Gson();
     private VoucherDAO voucherDAO=new VoucherDAO();
+    // tạo voucher mới
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        String urlPath=request.getServletPath();
-        switch (urlPath) {
-            case Constant.URL_VOUCHER_ADD:
-               addVoucher(request,response);
-            break;
-            case Constant.URL_VOUCHER_DELETE:
-                deleteVoucher(request,response);
-            break;
-            case Constant.URL_VOUCHER_GET:
-                getVoucher(request,response);
-            break;
-               
-        }
-    }
-    private void getVoucher(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        Map<String, Object> res = new HashMap<> ();
-        try {
-            int id=Integer.parseInt(request.getParameter("voucher_id"));
-            Voucher voucher = (Voucher) voucherDAO.getById(id);
-            JSONHelper.sendJsonAsResponse(response, 200, voucher);
-        } 
-        catch (NumberFormatException e) {
-                res.put("message", "bad request, check url params again");
-                response.setStatus(400);
-                JSONHelper.sendJsonAsResponse(response, 400, res);
-        }
-    }
-    private void deleteVoucher(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        Map<String, Object> res = new HashMap<> ();
-        try {
-            int id=Integer.parseInt(request.getParameter("voucher_id"));
-            boolean isSuccess = voucherDAO.deleteObject(id);
-            if (isSuccess) {
-                    res.put("message", "success");	
-                    JSONHelper.sendJsonAsResponse(response, 200, res);
-            }
-            else {
-                    res.put("message", "server's error");	
-                    JSONHelper.sendJsonAsResponse(response, 500, res);
-            }
-        } 
-        catch (NumberFormatException e) {
-                res.put("message", "bad request, check url params again");	
-                JSONHelper.sendJsonAsResponse(response, 400, res);
-        }
-        
-    }
-    private void addVoucher(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        Map<String, Object> res = new HashMap<> ();
+       Map<String, Object> res = new HashMap<> ();
+       
         try{
-            int id=Integer.parseInt(request.getParameter("voucher_id"));
-            int discount_amount=Integer.parseInt(request.getParameter("discount_amount"));
-            int expire_time=Integer.parseInt(request.getParameter("expire_time"));
-            Voucher voucher= new Voucher(id, discount_amount, expire_time);
+            StringBuilder json = (StringBuilder) request.getAttribute("json");
+            JSONObject jsonObject = new JSONObject(json.toString());
+            int discount_amount=jsonObject.getInt("discount_amount");
+            int expire_time=jsonObject.getInt("expire_time");
+            Voucher voucher= new Voucher(0, discount_amount, expire_time);
             boolean isSuccess = voucherDAO.addObject(voucher);
             if (isSuccess) {
                     res.put("message", "success");	
@@ -94,9 +50,48 @@ public class OperateWithVoucher extends HttpServlet {
             res.put("message", "bad request, check url params again");	
             JSONHelper.sendJsonAsResponse(response, 400, res);
         }
-
-        
     }
+    // xoá voucher theo id
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         Map<String, Object> res = new HashMap<> ();
+        try {
+            StringBuilder json = (StringBuilder) req.getAttribute("json");
+            Gson gson = new Gson();
+            JSONObject jsonObject = new JSONObject(json.toString());
+            int id=jsonObject.getInt("voucher_id");
+            boolean isSuccess = voucherDAO.deleteObject(id);
+            if (isSuccess) {
+                    res.put("message", "success");	
+                    JSONHelper.sendJsonAsResponse(resp, 200, res);
+            }
+            else {
+                    res.put("message", "server's error");	
+                    JSONHelper.sendJsonAsResponse(resp, 500, res);
+            }
+        } 
+        catch (NumberFormatException e) {
+                res.put("message", "bad request, check url params again");	
+                JSONHelper.sendJsonAsResponse(resp, 400, res);
+        }
+    }
+    // lấy voucher theo id
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> res = new HashMap<> ();
+        try {
+            int id=Integer.parseInt(req.getParameter("voucher_id"));
+            Voucher voucher = (Voucher) voucherDAO.getById(id);
+            JSONHelper.sendJsonAsResponse(resp, 200, voucher);
+        } 
+        catch (NumberFormatException e) {
+                res.put("message", "bad request, check url params again");
+                resp.setStatus(400);
+                JSONHelper.sendJsonAsResponse(resp, 400, res);
+        }
+    }
+    
+   
     @Override
     public String getServletInfo() {
         return "Short description";
